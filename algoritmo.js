d83b758fbf9b50a0ac6f804b853e58d1499e6095
cauasilveira999-hue <cauasilveira999@gmail.com>
2025-09-01 21:54:15 -0300

    Add files via upload

---
+++ algoritmo.js
@@ -0,0 +1,8 @@
+// fazendo cafe
+
+primeiroPasso = "1. Coloque agua na chaleira \n"
+segundoPasso = "2. Aqueça ate ferver \n"
+terceiroPasso = "3. Coloque cafe no filtro \n"
+quartoPasso = "4. Coloque agua quente no cafe Mellita\n"
+quintoPasso = "5. Sirva"
+console.log(primeiroPasso, segundoPasso, terceiroPasso, quartoPasso, quintoPasso)
---
+++ bolo.js
@@ -0,0 +1,21 @@
+// fazendo bolo
+massa1 = "MASSA 1: Comprar massa pronta \n"
+primeiroPasso = "1. Misturar a massa com ovos, leite, farinha e óleo \n"
+segundoPasso = "2. Bater a massa \n"
+terceiroPasso = "3. Bote no forno \n"
+quartoPasso = "4. Retire a massa \n"
+massa2e3 = "MASSA 2 & 3: Repetir isso mais duas vezes \n"
+
+recheio1 = "RECHEIO 1: Fazer um brigadeiro \n"
+recheio2 = "RECHEIO 2: Fazer uma ganache \n"
+recheio3 = "RECHEIO 3: Fazer um mousse \n"
+
+cobertura = "COBERTURA passo 1: Botar leite condensado numa vasilha \n"
+cobertura2 = "Passo 2: Bote creme de leite \n"
+cobertura3 = "Passo 3: Bote chocolate \n"
+cobertura4 = "Passo 4: Jogue em uma panela no fogão \n"
+cobertura5 = "Passo 5: Espere virar uma calda \n"
+
+montagem1 = "MONTAGEM: Monte o bolo e sirva"
+
+console.log(massa1, primeiroPasso,segundoPasso,terceiroPasso,quartoPasso,massa2e3,recheio1,recheio2,recheio3,cobertura,cobertura2,cobertura3,cobertura4,cobertura5,montagem1)
